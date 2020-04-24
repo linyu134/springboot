f0b29f42171fcc76cf5aeca39c585023d912e3ce
linyu134 <593553724@qq.com>
2020-04-24 21:01:44 +0800

    update

--- src/main/java/com/example/demo/Controller/indexController.java
+++ src/main/java/com/example/demo/Controller/indexController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class indexController {
 
+    //跳转到index界面
     @RequestMapping("/index")
     public String index(Model model) {
+        
         model.addAttribute("name", "index");
         return "index";
     }
     
+    //跳转到login界面
     @RequestMapping("/login")
     public String login() {
         return "redirect:login";
